795d3f76fae9398c200b1c4840bb6a8de33cf567
JewelKahz <67759825+JewelKahz@users.noreply.github.com>
2022-05-16 19:00:38 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+import HomeScreen from './src/screens/Home'
+
+
+export default function App() {
+  return (
+    <View>
+      <HomeScreen />
+    </View>
+  )
+}
+
