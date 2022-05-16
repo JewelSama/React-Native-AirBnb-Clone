f07018b29685f83004ea515364565d6b83664147
JewelKahz <67759825+JewelKahz@users.noreply.github.com>
2022-05-16 22:15:05 +0100

    List of props

--- App.js
+++ App.js
@@ -3,12 +3,14 @@ import React from 'react'
 import HomeScreen from './src/screens/Home'
 import Post from './src/components/post';
 import feed from './assets/data/feed';
+import SearchResultScreen from './src/screens/searchResults';
 
 export default function App() {
     const post2 = feed[2];
   return (
     <SafeAreaView>
-      <Post post={post2}/>
+      {/* <Post post={post2}/> */}
+      <SearchResultScreen />
     </SafeAreaView>
   )
 }
