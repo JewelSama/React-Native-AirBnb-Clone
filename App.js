96043afaa7ff1de6f66c190282c0d25223c67fbf
JewelKahz <67759825+JewelKahz@users.noreply.github.com>
2022-05-16 21:19:36 +0100

    Minor changes

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { View, Text, SafeAreaView } from 'react-native'
 import React from 'react'
 import HomeScreen from './src/screens/Home'
+import Post from './src/components/post';
 
 
 export default function App() {
   return (
     <View>
-      <HomeScreen />
+      <Post />
     </View>
   )
 }
