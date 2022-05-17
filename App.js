a32698e29a2794ad24bc9da53a0a8f7e88d07397
JewelKahz <67759825+JewelKahz@users.noreply.github.com>
2022-05-17 18:42:59 +0100

    navigation

--- App.js
+++ App.js
@@ -1,22 +1,14 @@
 import { View, Text, SafeAreaView } from 'react-native'
 import React from 'react'
-import HomeScreen from './src/screens/Home'
-import Post from './src/components/post';
-import feed from './assets/data/feed';
-import SearchResultScreen from './src/screens/searchResults';
+import Router from './src/navigation/Router';
 import DestinationSearch from './src/screens/destinationSearch';
-import GuestsScreen from './src/screens/Guests';
 
 
 export default function App() {
-    const post2 = feed[2];
   return (
-    <SafeAreaView>
-      {/* <Post post={post2}/> */}
-      {/* <SearchResultScreen /> */}
-      {/* <DestinationSearch /> */}
-      <GuestsScreen />
-    </SafeAreaView>
+    <>
+      <Router />
+    </>
   )
 }
 
