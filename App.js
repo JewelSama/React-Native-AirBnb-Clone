e08cfe594cc96505c2187d09ba39de8be90db0aa
JewelKahz <67759825+JewelKahz@users.noreply.github.com>
2022-05-17 13:08:58 +0100

    Search destination

--- App.js
+++ App.js
@@ -4,13 +4,15 @@ import HomeScreen from './src/screens/Home'
 import Post from './src/components/post';
 import feed from './assets/data/feed';
 import SearchResultScreen from './src/screens/searchResults';
+import DestinationSearch from './src/screens/destinationSearch';
 
 export default function App() {
     const post2 = feed[2];
   return (
     <SafeAreaView>
       {/* <Post post={post2}/> */}
-      <SearchResultScreen />
+      {/* <SearchResultScreen /> */}
+      <DestinationSearch />
     </SafeAreaView>
   )
 }
---
+++ src/screens/destinationSearch/index.js
@@ -0,0 +1,38 @@
+import { View, Text, TextInput, FlatList } from 'react-native'
+import React, {useState} from 'react'
+import styles from './styles';
+import searchResults from '../../../assets/data/search';
+import Entypo from 'react-native-vector-icons/Entypo';
+
+const DestinationSearch = () => {
+  const [inputText, setInputText] = useState();
+  return (
+    <View style={styles.container}>
+      {/* input */}
+      <TextInput 
+        style={styles.textInput} 
+          placeholder="Where are you going?"
+          value={inputText}
+          onChangeText={setInputText}
+          />
+ 
+    {/* list of destinations */}
+    <FlatList 
+      data={searchResults}
+      renderItem={({item}) => (
+        <View style={styles.row}>
+          <View style={styles.iconContainer}> 
+        <Entypo 
+          name={"location-pin"} size={30}
+        />
+        </View>
+        <Text style={styles.locationText}>{item.description}</Text>
+      </View>
+      )}
+    
+    />
+    </View>
+  )
+}
+
+export default DestinationSearch;
